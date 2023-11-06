2f8da0cb48cd19a55867b0fc914363f4badd4f34
congcm <congcm@techasians.com>
2023-11-06 22:39:38 +0700

    get state in store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import store from './store/store';
+import toDoReducer from './reducer/todoReducer';
 
 function App() {
+  const todoListState = useSelector(state => state.todo);
+  console.log('todoListState', todoListState);
   return (
     <>
       App component
