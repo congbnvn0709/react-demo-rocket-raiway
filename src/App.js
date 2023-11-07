5d8cced515354c2121a6e5d26d24854e913cc1da
congcm <congcm@techasians.com>
2023-11-07 19:40:31 +0700

    commit code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className='layout'>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1677758363734-aa22dfc62550?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://images.unsplash.com/photo-1677628624156-a224f23be035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://plus.unsplash.com/premium_photo-1677171215044-2dca78cce539?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://images.unsplash.com/photo-1677758311345-cc78867cdc0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+      </div> */}
+      <span>=========================================================</span>
+      <div className='layout2'>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1677758363734-aa22dfc62550?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div className='layout-item'>
+          <img src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://images.unsplash.com/photo-1677628624156-a224f23be035?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://plus.unsplash.com/premium_photo-1677171215044-2dca78cce539?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+        <div class="layout-item">
+          <img src="https://images.unsplash.com/photo-1677758311345-cc78867cdc0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" class="layout-image"></img>
+        </div>
+      </div>
+    </>
   );
 }
 
